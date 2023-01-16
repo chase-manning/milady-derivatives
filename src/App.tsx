1e24ffca9e4e2b02fa64089520b2752dea85e6ee
chase-manning <chase@manning.dev>
2023-01-16 14:50:20 +0000

    :rocket: add miladys to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import Milady from "./components/Milady";
 import TextArea from "./components/TextArea";
+import useMiladys from "./hooks/use-miladys";
 
 const StyledApp = styled.div`
   width: 100%;
@@ -11,10 +13,27 @@ const StyledApp = styled.div`
   padding: 4rem;
 `
 
+const Miladys = styled.div`
+  width: 100%;
+  padding: 6rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  grid-gap: 2rem;
+`
+
 function App() {
+  const miladys = useMiladys();
+
   return (
     <StyledApp>
       <TextArea header="Milady Derivatives" body="An unofficial collection of Milady Derivatives maintained by the community. To add or update a derivative please view the contribution guide." />
+      <Miladys>
+        {
+          miladys && miladys.map(milady => {
+            return <Milady milady={milady} />
+          })
+        }
+      </Miladys>
     </StyledApp>
   );
 }
