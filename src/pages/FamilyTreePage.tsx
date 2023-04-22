3d6eb4392052b65982497f3928f5cfa9a0e9ba7c
chase-manning <chase@manning.dev>
2023-04-22 08:45:33 +0100

    :sparkles: create family tree page

---
+++ src/pages/FamilyTreePage.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledFamilyTreePage = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+
+const FamilyTreePage = () => {
+  return <StyledFamilyTreePage>meow</StyledFamilyTreePage>;
+};
+
+export default FamilyTreePage;
